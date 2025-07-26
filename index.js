9f096e57e0595b6c83bd1b72b899d010f0c14dff
Milki Legesse <nitfanono@gmail.com>
2025-07-26 22:48:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express()
+app.use(express.json());
+app.post("/", (req, res ) => {
+    console.log(req.body);
+    return res.json({message: req.body})
+
+});
+
